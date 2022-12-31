f37e3d76887dd8a59f040d48fc8a255dd7151abf
minha <minhaz.ahmed@student.hs-rm.de>
2022-12-31 23:49:34 +0100

    day 2

--- Tag02/day02.py
+++ Tag02/day02.py
@@ -1,16 +1,53 @@
 def read_puzzle(file):
     with open(file) as f:
-        for line in f:
-            print(line)
-        return line
+        return [line.split() for line in f.read().split("\n")]
 
 
 def main():
     puzzle = read_puzzle("day02.txt")
 
+    score_part1 = 0
+    score_part2 = 0
+    rock, paper, scissor = 1, 2, 3
+    win = 6
+    draw = 3
+
+    for a, b in puzzle:
+        a = ord(a) - 64
+        b = ord(b) - 87
+        score_part1 += rock if b == rock else paper if b == paper else scissor
+        score_part2 += draw if b == paper else win if b == scissor else 0
+
+        if a == b:
+            score_part1 += draw
+        elif a == rock and b == paper or a == paper and b == scissor or a == scissor and b == rock:
+            score_part1 += win
+
+        if b == rock:
+            if a == rock:
+                score_part2 += scissor
+            elif a == paper:
+                score_part2 += rock
+            else:
+                score_part2 += paper
+        if b == paper:
+            if a == rock:
+                score_part2 += rock
+            elif a == paper:
+                score_part2 += paper
+            else:
+                score_part2 += scissor
+        if b == scissor:
+            if a == rock:
+                score_part2 += paper
+            elif a == paper:
+                score_part2 += scissor
+            else:
+                score_part2 += rock
+
+    print(score_part1, score_part2)
     return puzzle
 
 
 if __name__ == '__main__':
     main()
-
